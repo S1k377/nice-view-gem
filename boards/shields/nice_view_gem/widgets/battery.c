1f9568a07a712d53b441e472ba0987abdd53df47
S1k377 <sikemetalcurse@gmail.com>
2025-05-20 18:43:24 +0200

    position v0.4

--- boards/shields/nice_view_gem/widgets/battery.c
+++ boards/shields/nice_view_gem/widgets/battery.c
@@ -2,11 +2,12 @@
 #include "battery.h"
 #include "../assets/custom_fonts.h"
 
+
 LV_IMG_DECLARE(bolt);
 
 static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_right_dsc;
-    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
 
     char text[10] = {};
 
@@ -18,13 +19,13 @@ static void draw_charging_level(lv_obj_t *canvas, const struct status_state *sta
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
     lv_draw_label_dsc_t label_right_dsc;
-    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
 
     char text[10] = {};        
 
     sprintf(text, "%i%%", state->battery);
     lv_canvas_draw_text(canvas, 0, 1, 25, &label_right_dsc, text);
-    lv_canvas_draw_img(canvas, 0, 40, &bolt, &img_dsc);
+    lv_canvas_draw_img(canvas, 38, 1, &bolt, &img_dsc);
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
